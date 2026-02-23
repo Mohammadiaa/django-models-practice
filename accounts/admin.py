21aa138acc0489218c26c0fd1819791965c9bf1d
Mohammadi <amohammadia0086@gmail.com>
2026-02-23 19:56:08 +0330

    Register Profile model in admin
    Added ProfileAdmin to manage the Profile model in the Django admin.
    Configured list_display, list_filter, and search_fields for easier management.
    Since the default Django User model is used, no CustomUserAdmin is needed.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import Profile
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "phone_number", "level"]
+    list_filter = ["level"]
+    search_fields = ["user__username", "phone_number"]
+
+admin.site.register(Profile, ProfileAdmin)
