72bab78fa7974635da44de84345945041a59e64d
Mohammadi <amohammadia0086@gmail.com>
2026-02-24 23:03:42 +0330

    add Team admin interface
    - Added TeamAdmin to manage Team model in Django Admin
    - Configured list_display, search_fields, list_filter, and filter_horizontal for members
    - ProfileAdmin already configured for user profiles

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Team
+
 # Register your models here.
 
 
@@ -9,4 +10,13 @@ class ProfileAdmin(admin.ModelAdmin):
     list_filter = ["level"]
     search_fields = ["user__username", "phone_number"]
 
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'foundation', 'leader')
+    search_fields = ('name', 'leader__username')
+    list_filter = ('foundation',)
+    filter_horizontal = ('members',) 
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Team, TeamAdmin)
