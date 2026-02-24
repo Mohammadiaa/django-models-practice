370f35f2396a2fb3f4373f4a340fb192217d5cbc
Mohammadi <amohammadia0086@gmail.com>
2026-02-24 23:11:54 +0330

    add Project model and initial migration
    - Added Project model with title, description, and ForeignKey to Team
    - on_delete=CASCADE ensures projects are deleted if the team is removed
    - Created initial migration for Project

--- accounts/models.py
+++ accounts/models.py
@@ -36,3 +36,13 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    team = models.ForeignKey(Team, related_name="projects", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    
